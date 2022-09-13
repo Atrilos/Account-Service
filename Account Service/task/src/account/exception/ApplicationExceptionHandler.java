650daf7db42dd51305a12a5d20af1bacf6046a09
Atrilos <101889118+Atrilos@users.noreply.github.com>
2022-09-14 02:38:22 +0700

    Stage 2/7 done
    -Change dependency from hibernate-validator to ..starter-validator
    -Change exception handler to handle @Valid exceptions

--- Account Service/task/src/account/exception/ApplicationExceptionHandler.java
+++ Account Service/task/src/account/exception/ApplicationExceptionHandler.java
@@ -1,19 +1,25 @@
 package account.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestControllerAdvice
-public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+public class ApplicationExceptionHandler {
 
     @ExceptionHandler(UserExistException.class)
     public void handleUserExistException(UserExistException ex,
                                          HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(BindException.class)
+    public void handleBindException(BindException ex,
+                                    HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
     }
 }
